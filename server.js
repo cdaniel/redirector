62c33dabd68fe22008ec79065cb12b7b7e020e5e
Chris <krzysztof.daniel@gmail.com>
2020-12-04 14:10:38 +0100

    Migration to the IBM Certificate manager and some cleanup
    http://wardleymaps.com was not working earlier. Now it seems to work.

--- server.js
+++ server.js
@@ -4,114 +4,122 @@ var cfenv = require("cfenv");
 
 
 var redirector = require('redirect-rules');
-var lefMappingPage = 'https://leadingedgeforum.com/advisory/wardley-mapping/';
+var lefMappingPage = 'https://leadingedgeforum.com/insights/filter/wardley-mapping';
 
 
 var rules = [
+
     {
-    	from: { 
+    	from: {
+    		protocol: 'http',
+    		hostname: 'www.wardleymaps.com'
+    	},
+        to: lefMappingPage,
+        status: 301
+    },
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname: 'www.wardleymaps.com'
+    	},
+        to: lefMappingPage,
+        status: 301
+    },
+
+
+    {
+    	from: {
+    		protocol: 'http',
+    		hostname: 'atlas.wardleymaps.com'
+    	},
+        to: 'https://atlas2.wardleymaps.com',
+        status: 301
+    },
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname: 'atlas.wardleymaps.com'
+    	},
+        to: 'https://atlas2.wardleymaps.com',
+        status: 301
+    },
+
+    {
+    	from: {
+    		protocol: 'http',
+    		hostname: 'preview.wardleymaps.com'
+    	},
+        to: 'https://atlas2.wardleymaps.com',
+        status: 301
+    },
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname: 'preview.wardleymaps.com'
+    	},
+        to: 'https://atlas2.wardleymaps.com',
+        status: 301
+    },
+
+
+    {
+    	from: {
+    		protocol: 'http',
+    		hostname: 'learn.wardleymaps.com'
+    	},
+        to: 'https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424',
+        status: 301
+    },
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname: 'learn.wardleymaps.com'
+    	},
+        to: 'https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424',
+        status: 301
+    },
+
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname : "redirector.eu-gb.mybluemix.net"
+    	},
+    	to: 'https://leadingedgeforum.com/advisory/wardley-mapping/'
+    },
+    {
+    	from: {
+    		protocol: 'https',
+    		hostname : "redirector.eu-gb.mybluemix.net"
+    	},
+    	to: 'https://leadingedgeforum.com/advisory/wardley-mapping/'
+    },
+
+
+    {
+    	from: {
+    		protocol: 'http',
+    		hostname : "doctrine.wardleymaps.com"
+    	},
+    	to: 'https://doctrine.wardleymaps.com/'
+    },
+
+    {
+    	from: {
     		protocol: 'http',
     		hostname: 'wardleymaps.com'
     	},
-        to: 'https://leadingedgeforum.com/advisory/wardley-mapping/',
+        to: lefMappingPage,
         status: 301
     },
-    { 
+    {
     	from: {
     		protocol: 'https',
     		hostname : "wardleymaps.com"
-    	}, 
-    	to: '//leadingedgeforum.com/advisory/wardley-mapping/'
-    },
-    
-    
-    
-    
-    {
-    	from: { 
-    		protocol: 'http',
-    		hostname: 'www.wardleymaps.com'
     	},
-        to: 'https://leadingedgeforum.com/advisory/wardley-mapping/',
-        status: 301
+    	to: lefMappingPage,
+      status: 301
     },
-    {
-    	from: { 
-    		protocol: 'https',
-    		hostname: 'www.wardleymaps.com'
-    	},
-        to: 'https://leadingedgeforum.com/advisory/wardley-mapping/',
-        status: 301
-    },
-    
-    
-    {
-    	from: { 
-    		protocol: 'http',
-    		hostname: 'atlas.wardleymaps.com'
-    	},
-        to: 'https://atlas2.wardleymaps.com',
-        status: 301
-    },
-    {
-    	from: { 
-    		protocol: 'https',
-    		hostname: 'atlas.wardleymaps.com'
-    	},
-        to: 'https://atlas2.wardleymaps.com',
-        status: 301
-    },
-    
-    {
-    	from: { 
-    		protocol: 'http',
-    		hostname: 'preview.wardleymaps.com'
-    	},
-        to: 'https://atlas2.wardleymaps.com',
-        status: 301
-    },
-    {
-    	from: { 
-    		protocol: 'https',
-    		hostname: 'preview.wardleymaps.com'
-    	},
-        to: 'https://atlas2.wardleymaps.com',
-        status: 301
-    },
-    
-    
-    {
-    	from: { 
-    		protocol: 'http',
-    		hostname: 'learn.wardleymaps.com'
-    	},
-        to: 'https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424',
-        status: 301
-    },
-    {
-    	from: { 
-    		protocol: 'https',
-    		hostname: 'learn.wardleymaps.com'
-    	},
-        to: 'https://learn.leadingedgeforum.com/p/wardley-mapping/?product_id=277424',
-        status: 301
-    },
-    
-    { 
-    	from: {
-    		protocol: 'https',
-    		hostname : "redirector.eu-gb.mybluemix.net"
-    	}, 
-    	to: 'https://leadingedgeforum.com/advisory/wardley-mapping/'
-    },
-    { 
-    	from: {
-    		protocol: 'https',
-    		hostname : "redirector.eu-gb.mybluemix.net"
-    	}, 
-    	to: 'https://leadingedgeforum.com/advisory/wardley-mapping/'
-    },
-    
+
 ];
 app.use(redirector(rules));
 
